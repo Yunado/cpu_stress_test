e2cd9d130c6ebe8f70cefcda59494fd9c26bec43
Yunfei Ouyang <36465036+Yunado@users.noreply.github.com>
2023-05-09 00:23:53 -0400

    stress cpu

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def stress_cpu():
+    # Run stress_cpu.py in a separate process
+    subprocess.Popen(["python3", "stress_cpu.py"], stdout=subprocess.PIPE)
+    return "Started stressing CPU\n"
+
+
+@app.route("/", methods=["GET"])
+def get_ip():
+    # Return private IP address of EC2 instance
+    return socket.gethostname()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
